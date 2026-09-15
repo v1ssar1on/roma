ae26e592c73aab34e424a92846eae3f685fb1885
v1ssar1on <edward.kondr@bk.ru>
2026-09-15 15:55:14 +0300

    upd

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { RoomModule } from './room/room.module';
 
 @Module({
   imports: [RoomModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
